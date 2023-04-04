8a76acf03754ee2a52aecf9634823942321b985d
Guti <cristiang52001@hotmail.com>
2023-04-04 13:28:01 -0400

    bunch of stuff

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ const results = document.getElementById("results");
 let players = [];
 let previousQuery = "";
 
-async function fetchPlayers() {
+async function fetchPlayersData() {
   const proxyUrl = "https://api.allorigins.win/raw?url=";
   const apiUrl = "https://fantasy.premierleague.com/api/bootstrap-static/";
   const response = await fetch(proxyUrl + encodeURIComponent(apiUrl));
@@ -26,28 +26,6 @@ async function fetchPlayers() {
       owned: player.selected_by_percent,
     };
   });
-
-  const teamNameMap = {
-    ARS: "Arsenal",
-    AVL: "Aston Villa",
-    BRE: "Brentford",
-    BHA: "Brighton & Hove Albion",
-    BUR: "Burnley",
-    CHE: "Chelsea",
-    CRY: "Crystal Palace",
-    EVE: "Everton",
-    LEI: "Leicester City",
-    LIV: "Liverpool",
-    MCI: "Manchester City",
-    MUN: "Manchester United",
-    NEW: "Newcastle United",
-    NOR: "Norwich City",
-    SOU: "Southampton",
-    TOT: "Tottenham Hotspur",
-    WAT: "Watford",
-    WHU: "West Ham United",
-    WOL: "Wolverhampton Wanderers",
-  };
 }
 
 function filterPlayers(query) {
@@ -61,44 +39,85 @@ function filterPlayers(query) {
     .sort((a, b) => b.points - a.points);
 }
 
+async function showPlayerDetails(player) {
+  // Clear the results container
+  results.innerHTML = "";
+
+  // Create a container for the player details
+  const playerDetailsContainer = document.createElement("div");
+  playerDetailsContainer.classList.add("player-details");
+
+  // Add player name
+  const playerNameElement = document.createElement("h2");
+  playerNameElement.textContent = `${player.firstName} ${player.lastName}`;
+  playerDetailsContainer.appendChild(playerNameElement);
+
+  // Add player image
+  const playerImage = document.createElement("img");
+  playerImage.src = playerPhoto;
+  playerImage.alt = `${player.firstName} ${player.lastName}`;
+  playerImage.width = 110;
+  playerImage.height = 140;
+
+  playerImage.addEventListener("error", (e) => {
+    console.error("Error loading player image:", e);
+  });
+
+  playerDetailsContainer.appendChild(playerImage);
+
+  // Add more player details
+  // For example: player.position, player.team, player.points, etc.
+  // Customize this section based on the data you want to display.
+
+  // Append the player details container to the results container
+  results.appendChild(playerDetailsContainer);
+
+  // Add a back button to return to the search results
+  const backButton = document.createElement("button");
+  backButton.textContent = "Back";
+  backButton.classList.add("back-button");
+  backButton.addEventListener("click", () => {
+    displayPlayers(filterPlayers(previousQuery));
+  });
+
+  results.appendChild(backButton);
+}
+
 function displayPlayers(filteredPlayers) {
   results.innerHTML = "";
+
   if (filteredPlayers.length > 0) {
     const headerRow = document.createElement("div");
     headerRow.classList.add("player-row", "header-row");
     headerRow.innerHTML = `
-      <span>Name</span>
-      <span>Team</span>
-      <span>POS</span>
-      <span>Points</span>
-      <span>Owned</span>
-    `;
+        <span>Name</span>
+        <span>Team</span>
+        <span>POS</span
+        <span>Points</span>
+        <span>Owned</span>
+      `;
     results.appendChild(headerRow);
   }
+
   filteredPlayers.forEach((player) => {
     const playerRow = document.createElement("div");
     playerRow.classList.add("player-row");
     playerRow.innerHTML = `
-      <span title="${player.firstName} ${
+        <span title="${player.firstName} ${
       player.lastName
     }">${player.firstName.charAt(0)}. ${player.lastName}</span>
-      <span title="${player.team}">${player.team}</span>
-      <span>${player.pos}</span>
-      <span>${player.points} Pts</span>
-      <span>${player.owned} %</span>
-    `;
+        <span title="${player.team}">${player.team}</span>
+        <span>${player.pos}</span>
+        <span>${player.points} Pts</span>
+        <span>${player.owned} %</span>
+      `;
+
+    playerRow.addEventListener("click", () => {
+      showPlayerDetails(player);
+    });
+
     results.appendChild(playerRow);
   });
-
-  playerRow.innerHTML = `
-  <span title="${player.firstName} ${
-    player.lastName
-  }">${player.firstName.charAt(0)}. ${player.lastName}</span>
-  <span title="${player.teamFullName}">${player.team}</span>
-  <span>${player.pos}</span>
-  <span>${player.points} Pts</span>
-  <span>${player.owned} %</span>
-`;
 }
 
 search.addEventListener("input", (e) => {
@@ -116,4 +135,4 @@ search.addEventListener("focus", () => {
 });
 
 // Fetch player data
-fetchPlayers();
+fetchPlayersData();
